f9fc7eba83f16b78734ec51007a2c4cf6648e5e1
Nicolò Taormina <nicktaormina3@gmail.com>
2023-08-10 13:12:37 +0200

    implemented particle system found bug with coordinates(?) need to resolve

--- Assets/Scripts/LivelyCamera.cs
+++ Assets/Scripts/LivelyCamera.cs
@@ -7,11 +7,12 @@ public class LivelyCamera : MonoBehaviour
         springStrength = 100f,
         dampingStrength = 10f,
         jostleStrength = 40f,
-        pushStrength = 1f;
+        pushStrength = 1f,
+        maxDeltaTime = 1f / 60f;
 
     Vector3 anchorPosition, velocity;
 
-    private void Awake() => anchorPosition = transform.localPosition;
+    void Awake() => anchorPosition = transform.localPosition;
 
     public void JostleY() => velocity.y += jostleStrength;
 
@@ -21,11 +22,22 @@ public class LivelyCamera : MonoBehaviour
         velocity.z += pushStrength * impulse.y;
     }
 
-    private void LateUpdate()
+    void LateUpdate()
+    {
+        float dt = Time.deltaTime;
+        while (dt > maxDeltaTime)
+        {
+            TimeStep(maxDeltaTime);
+            dt -= maxDeltaTime;
+        }
+        TimeStep(dt);
+    }
+
+    void TimeStep(float dt)
     {
         Vector3 displacement = anchorPosition - transform.localPosition;
         Vector3 acceleration = springStrength * displacement - dampingStrength * velocity;
-        velocity += acceleration * Time.deltaTime;
-        transform.localPosition += velocity * Time.deltaTime;
+        velocity += acceleration * dt;
+        transform.localPosition += velocity * dt;
     }
 }
